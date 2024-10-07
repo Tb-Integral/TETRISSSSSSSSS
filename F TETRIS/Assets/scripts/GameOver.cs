2c353cc3cd511bac95dec8d0704d675092d18750
Mr-Triangle <dox.cactus@yandex.ru>
2024-10-07 15:28:48 +0500

    game over (menu button)
    and black bg

--- F TETRIS/Assets/scripts/GameOver.cs
+++ F TETRIS/Assets/scripts/GameOver.cs
@@ -2,10 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameOver : MonoBehaviour
 {
     [SerializeField] private GameObject _go;
+    [SerializeField] private GameObject _menuButton;
+    [SerializeField] private GameObject _black;
 
     public bool IsGameOver(Transform[,] grid)
     {
@@ -15,9 +18,16 @@ public class GameOver : MonoBehaviour
             {
                 Debug.Log("Game Over!");
                 _go.SetActive(true);
+                _menuButton.SetActive(true);
+                _black.SetActive(true);
                 return true;
             }
         }
         return false;
+    }
+
+    public void returnToMenu()
+    {
+        SceneManager.LoadScene("menu");
     }
 }
